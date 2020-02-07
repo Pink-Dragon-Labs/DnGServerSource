77fd024626cbbe6e73cd1ebfcd1973e13c4a368d
Caleb Hulbert <cmhulbert@gmail.com>
2020-02-06 19:53:59 -0500

    rename to match start script

--- integrationScripts/copyBinariesToLive.sh
+++ integrationScripts/copyBinariesToLive.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-cp $SERVER_BUILD/router $REALMFOLDER/router
+cp $SERVER_BUILD/router $REALMFOLDER/dngrouter
 chmod +x $REALMFOLDER/router
-cp $SERVER_BUILD/updatesrv $REALMFOLDER/update
+cp $SERVER_BUILD/updatesrv $REALMFOLDER/dngupdate
 chmod +x $REALMFOLDER/update
-cp $SERVER_BUILD/datamgr $REALMFOLDER/datamgr
+cp $SERVER_BUILD/datamgr $REALMFOLDER/dngdatamgr
 chmod +x $REALMFOLDER/datamgr
 cp $SERVER_BUILD/roommgr $REALMFOLDER/dawn/bin/main
 chmod +x $REALMFOLDER/dawn/bin/main
